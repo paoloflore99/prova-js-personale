4e20ccf031867c3a0913553302a9f9ec27b5223d
paoloflore99 <paoloflore937@gmail.com>
2023-07-12 22:13:29 +0200

    personale

--- myscript.js
+++ myscript.js
@@ -10,33 +10,81 @@
     tipologgia di commento
 */
 
-/*
+
 //consol
 console.log("riga js 15");  //stanpa nella consol
 
-//document
-document.getElementById("provaelemnt").innerHTML = "<h3>titolo modificato da js</h3>"
-//grazie a questo document posso andare a modificare html , soprasrivendolo.
 
+//document
+//grazie a questo document posso andare a modificare html , soprasrivendolo. 
+document.getElementById("primamodifica").innerHTML = "<h1>MODIFICATO DA JAVASCRIPT</h1>"
+
+
+//allert
 //allert-popup che compare 
 alert("ciao sono un allert")
 
-//variabbili
-/*
-la variabbile e un contenuto nel quale salvo dei dati
-*/
+
+//consol.dir 
+//lo uso per ispezzionare gli elementi da getelementbyid
+console.dir(document.getElementById("primamodifica"))
+
+
+
+//variabili
+//variabbili , cvi sono 3 tipi di variabbili   const  ,  let   ,  var   .
+//const le informazzioni messe allinterno non posso poi piu essere modificate 
+//Let Una variabile inizializzata con la keyword let è riassegnabile.
+//var e sempre preferibile utilizzare let.
+//esempio
+
+//dichiarazione variabbile
+let miaStringa;            
+
+//asegnazione variabbile
+miaStringa = "ciao"
+
+//utilizzo/richiamo 
+///alert(miaStringa)
+
+//asegnazione
+miaStringa = "Arrivederci"
+
+//utilizzo/richiamo 
+///alert (miaStringa)
+
+
+
+//Variabbile  creo una varibbile vuota ( sotto ce il continio )
+let provavariabbile;
+
+//per poi riprenderla qua  asegnadoli un valo 
+provavariabbile = 4;
+
+//provadella variabbile asegnta dopo
+///alert (provavariabbile)
+
+//const devo dare subbito il valore 
+const provacostante = 10;
+
+//tipologgia di variabbili
+//Number        numro
+//string        stringa, sequenza di caratteri
+//boolean       true fols  1 0   vero falso
+//object        ogetto
+//function      funzione un ogetto <speciale>
+//undefined     non definito
+//null          valore nullo
+
+
+
+                                                        //ancora da finire
 
 
 
 
 
-
-
-
-
-
-
-
+//riga(commento ) 66 65 48     video lezzione del (29 giugno)
 //-------------------------------------lezzione di oggi da mettere alla  fine---------------------------------------------------------
 
 /*
@@ -75,105 +123,10 @@ che permette di ricevere dei dati esterni e usarli internamente come funzione .
 gli argomenti sono come delle variabili senza valore . il valore asegna quando viene invocata
 */
 
-function name(arg1, arg2) {
+///function name(arg1, arg2) {
     
-}
+///}
 
 //invocazione
 //in questo modo asegno i valori degli argomenti delle funzioni
-nome("valore arg1", "valore arg2")
-
-
-//return
-
-
-
-
-
-
-
-
-//-----------------------------------liv codice-------------------------------------------------------------
-
-/*
-//serve per invertire le paore  e metterla a confronto con quella non capololta
-function isPalindroma(word) {
-    const letters = word.split("");
-
-    const reverseLetters = [];
-
-    for (let i = 0; i < array.length; i++) {
-        reverseword = reverseLetters[i];
-        
-    }
-
-    console.log(reverseLetters, reverseword)
-
-
-}
-
-const result = isPalindroma("ciao")
-
-
-*/
-//--------------------------------------seconda parte live--------------------------------------------
-
-/**
- * @type{HTMLSelectElement} 
- */
-const squareCountsSelect = document.querySelector("[name='squareCounts']")
-
-const btnStart = document.querySelector("#btn-start")
-
-const gridContainer = document.querySelector(".grid-container")
-
-
-
-btnStart.addEventListener("click", onBtnClick)
-
-
-function onBtnClick() {//codice che esegue il click
-
-    const squareCounts = parseInt(squareCountsSelect.value);//leggo il valore della sect
-
-    console.log("valore sclto", squareCounts)
-
-    //devo generare la griglia
-    const gridList = createGrid() //variabbile del pulsante
-
-    console.log ("gridList");
-}
-
-function createSingleSquare() {
-    const square = document.createElement("div");
-
-    square.classList.add("grid-square");
-    square.textContent = squareCount;
-    //output
-    return square;
-    square.classList
-    square
-    square
-    
-}
-
-
-/**
- * dato un numero di celle crea una griglia
- * @param {number} squareNumber numero quadrati de creare 
- * @returns {HTMLDivElement}
- */
-function createGrid(squareNumber) {
-    for (let I = 0; I < squareNumber; I++) {
-        const newSquare = createSingleSquare("sqr", + I);
-        
-        gridContainer.push(newSquare)
-    }
-
-
-    //output 
-    return grid;
-}
-
-
-createGrid()//qui specifico quanti quadrati voglio
+///nome("valore arg1", "valore arg2")
